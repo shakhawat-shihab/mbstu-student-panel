fd042cb48d6b4cabf3ab662cbc5f3fd7897fa91a
SHAKHAWAT <shihabchtg@gmail.com>
2022-11-07 19:12:47 +0600

    insert

--- src/Pages/Students/StudentCourseRegistration/StudentCourseRegistrationDetails/CourseRegistrationView/Payment.js
+++ src/Pages/Students/StudentCourseRegistration/StudentCourseRegistrationDetails/CourseRegistrationView/Payment.js
@@ -3,43 +3,43 @@ import { Button, Modal } from 'react-bootstrap';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import CheckoutForm from './CheckoutForm';
+import { Redirect, useHistory } from 'react-router-dom';
 const stripePromise = loadStripe('pk_test_51Kpnq6JmlJsO1FghhmfVHTrBjRtusmHHRlpxnxB9qrfYytcOTiyXaWP9y0BwHCRIqB15BV94yETiN677dhvwcpXE00FL3CpyUK');
 
 const Payment = (props) => {
     const [show, setShow] = useState(false);
     const { application } = props;
+    const history = useHistory();
     const amount = 10;
+    const processPayment = () => {
+        console.log('processPayment')
+        fetch('http://localhost:5000/api/v1/payment/ssl-init', {
+            method: 'put',
+            // { redirect: 'follow', 'content-type': 'Access-Control-Allow-Origin' }
+            headers: {
+                'Content-type': 'application/json',
+                'Authorization': `Bearer ${JSON.parse(localStorage.getItem('jwt'))}`,
+            },
+        }
+        )
+            .then(res => res.json())
+            .then(info => {
+                console.log('message ==> ', info);
+                window.location.href = info.url;
+            })
+    }
 
     return (
         <>
-            <Modal
-                show={show}
-                onHide={() => { setShow(false) }}
-                backdrop="static"
-                keyboard={false}
-                centered
-            >
-                <Modal.Header closeButton>
-                    <Modal.Title>Complete your payment</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    Total Amount {amount} $
-                    {
-                        amount
-                            ?
-                            <Elements stripe={stripePromise}>
-                                <CheckoutForm application={application} />
-                            </Elements>
-                            :
-                            <p>Payment can't be processed now</p>
-                    }
-                </Modal.Body>
-
-
-            </Modal>
             <div className='text-center'>
                 <h2>Payment View</h2>
-                <Button variant='warning' onClick={() => setShow(true)}>Pay</Button>
+                <Button variant='warning'
+                    onClick={() => {
+                        // setShow(true)
+                        processPayment();
+                    }}>
+                    Pay
+                </Button>
             </div>
         </>
 
