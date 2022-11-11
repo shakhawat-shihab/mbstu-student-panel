3f2735f113780137200b11754050ab159d647fa6
SHAKHAWAT <shihabchtg@gmail.com>
2022-11-11 17:41:21 +0600

    pagination

--- src/Pages/Students/StudentCourseRegistration/StudentCourseRegistrationDetails/CourseRegistrationView/AdmitCard.js
+++ src/Pages/Students/StudentCourseRegistration/StudentCourseRegistrationDetails/CourseRegistrationView/AdmitCard.js
@@ -2,14 +2,12 @@ import React, { useState } from 'react';
 import { useEffect } from 'react';
 import { Button } from 'react-bootstrap';
 import checkDepartmentName from '../../../../../Functions/DeptCodeToDeptName';
-import useAuth from '../../../../../Hooks/useAuth';
 import mbstuLogo from "../../../../../images/mbstu-logo.jpg"
 import './AdmitCard.css'
 import AdmitCardModal from './AdmitCardModal';
 
 const AdmitCard = (props) => {
 
-    const { user } = useAuth();
     const { application } = props;
 
     const { name, degree, applicantId, applicantName, applicantSession, department,
@@ -17,8 +15,6 @@ const AdmitCard = (props) => {
 
     const [profile, setProfile] = useState();
     const [showModal, setShowModal] = useState(false);
-
-    console.log("admit applicationn === ", application);
 
     useEffect(() => {
         fetch("http://localhost:5000/api/v1/profile", {
@@ -29,103 +25,108 @@ const AdmitCard = (props) => {
         })
             .then(res => res.json())
             .then(info => {
-                console.log("profile info == ", info)
                 setProfile(info?.data);
             })
     }, [])
+
     return (
         <div>
             <div>
                 <AdmitCardModal application={application} showModal={showModal} setShowModal={setShowModal} />
             </div>
 
-            <div className='container shadow-lg  py-5 px-5 my-4 rounded'>
+            {application?.isAcademicCommitteeVerified == true
+                ?
+                <div className='container shadow-lg  py-5 px-5'>
 
-                <h2 className='text-center mb-5' style={{ color: "#3C3FED" }}>Admit Card</h2>
+                    <h2 className='text-center mb-5 me-5' style={{ color: "#3C3FED" }}>Admit Card</h2>
 
-                <div className='px-4'>
-                    <div className='row'>
-                        <div className='col-lg-1 mt-2'>
-                            <img src={mbstuLogo} alt="mbstu" style={{ width: "100px" }} />
+                    <div className='px-4'>
+                        <div className='row'>
+                            <div className='col-lg-1 mt-2'>
+                                <img src={mbstuLogo} alt="mbstu" style={{ width: "140px" }} />
+                            </div>
+                            <div className="col-lg-9  mt-2 font-style-work-sans">
+                                <h3 className='fw-bold text-center' style={{ fontSize: "22px" }}>Mawlana Bhashani Science and Technology University</h3>
+                                <p className='text-center'>Santosh, Tangail</p>
+                                <h3 className='text-center mb-3'>Admit Card</h3>
+                                <h5 className='text-center'>{name} {degree} Final Examination</h5>
+                            </div>
+                            <div className='col-lg-2'>
+                                <img src={profile?.imageURL} alt="myself" style={{ width: "170px", height: "170px" }} />
+                            </div>
                         </div>
-                        <div className="col-lg-9 ps-4 mt-2 font-style-work-sans">
-                            <h3 className='fw-bold text-center mb-1' style={{ wordSpacing: "5px" }}>Mawlana Bhashani Science and Technology University</h3>
-                            <p className='text-center'>Santosh, Tangail</p>
-                            <h3 className='text-center fw-bold mb-3'>Admit Card</h3>
-                            <h5 className='text-center'>{name} {degree} Final Examination</h5>
+
+                        <div className='d-flex flex-column mb-3 mt-3'>
+                            <span style={{ fontSize: "16px" }}><span className="fw-bold">Student ID: </span>{applicantId.toUpperCase()}</span>
+                            <span style={{ fontSize: "16px" }}><span className="fw-bold">Session: </span>{applicantSession}</span>
+                            <span style={{ fontSize: "16px" }}><span className="fw-bold">Name of the applicant: </span>{applicantName}</span>
+
                         </div>
-                        <div className='col-lg-2'>
-                            <img src={profile?.imageURL} alt={`picture of ${applicantName} `} style={{ width: "170px", height: "170px" }} />
+
+                        <div className='d-flex flex-column mb-3'>
+                            <span style={{ fontSize: "16px" }}><span className="fw-bold">Name of the Department: </span>{checkDepartmentName(department)}</span>
+                            <span style={{ fontSize: "16px" }}><span className="fw-bold">Staring Date of Examination: </span></span>
+                            <span style={{ fontSize: "16px" }}><span className="fw-bold">Name of the Hall: </span>{applicantHallName}</span>
+                        </div>
+
+                        <div>
+                            {
+                                regularCourses?.length !== 0 &&
+                                <div className='my-2'>
+                                    <span className='fw-bold' style={{ fontSize: "16px" }}>Regular Courses: </span> <br />
+                                    <ol>
+                                        {
+                                            regularCourses?.map(x => <li>{x?.courseCode?.toUpperCase()}</li>)
+                                        }
+                                    </ol>
+                                </div>
+                            }
+                            {
+                                backlogCourses?.length !== 0 &&
+                                <div className='my-2'>
+                                    <span className='fw-bold' style={{ fontSize: "16px" }}>Backlog Courses: </span> <br />
+                                    <ol>
+                                        {
+                                            backlogCourses?.map(x => <li>{x?.courseCode?.toUpperCase()}</li>)
+                                        }
+                                    </ol>
+                                </div>
+                            }
+                            {
+                                specialCourses?.length !== 0 &&
+                                <div className='my-2'>
+                                    <span className='fw-bold' style={{ fontSize: "16px" }}>Special Courses: </span> <br />
+                                    <ol>
+                                        {
+                                            specialCourses?.map(x => <li>{x?.courseCode?.toUpperCase()}</li>)
+                                        }
+                                    </ol>
+                                </div>
+                            }
+
+                            <div className="mt-5 pe-3 d-flex flex-row-reverse">
+                                <div>
+                                    <hr style={{ height: "3px", color: "black", bordr: "none" }} />
+                                    <p className='text-center'>Conroller of Examinations</p>
+                                </div>
+
+
+                            </div>
+
+
                         </div>
                     </div>
 
-                    <div className='d-flex flex-column mb-3'>
-                        <span style={{ fontSize: "16px" }}><span className="fw-bold">Student ID: </span>{applicantId.toUpperCase()}</span>
-                        <span style={{ fontSize: "16px" }}><span className="fw-bold">Session: </span>{applicantSession}</span>
-                        <span style={{ fontSize: "16px" }}><span className="fw-bold">Name of the applicant: </span>{applicantName}</span>
-
-                    </div>
-
-                    <div className='d-flex flex-column mb-3'>
-                        <span style={{ fontSize: "16px" }}><span className="fw-bold">Name of the Department: </span>{checkDepartmentName(department)}</span>
-                        <span style={{ fontSize: "16px" }}><span className="fw-bold">Staring Date of Examination: </span></span>
-                        <span style={{ fontSize: "16px" }}><span className="fw-bold">Name of the Hall: </span>{applicantHallName}</span>
-                    </div>
-
-                    <div>
-                        {
-                            regularCourses?.length !== 0 &&
-                            <div className='my-2'>
-                                <span className='fw-bold' style={{ fontSize: "16px" }}>Regular Courses: </span> <br />
-                                <ol>
-                                    {
-                                        regularCourses?.map(x => <li>{x?.courseCode?.toUpperCase()}</li>)
-                                    }
-                                </ol>
-                            </div>
-                        }
-                        {
-                            backlogCourses?.length !== 0 &&
-                            <div className='my-2'>
-                                <span className='fw-bold' style={{ fontSize: "16px" }}>Backlog Courses: </span> <br />
-                                <ol>
-                                    {
-                                        backlogCourses?.map(x => <li>{x?.courseCode?.toUpperCase()}</li>)
-                                    }
-                                </ol>
-                            </div>
-                        }
-                        {
-                            specialCourses?.length !== 0 &&
-                            <div className='my-2'>
-                                <span className='fw-bold' style={{ fontSize: "16px" }}>Special Courses: </span> <br />
-                                <ol>
-                                    {
-                                        specialCourses?.map(x => <li>{x?.courseCode?.toUpperCase()}</li>)
-                                    }
-                                </ol>
-                            </div>
-                        }
-
-                        <div className="mt-5 me-5 pe-5 d-flex flex-row-reverse">
-                            <div>
-                                <hr style={{ height: "3px", color: "black", bordr: "none" }} />
-                                <p className='text-center'>Conroller of Examinations</p>
-                            </div>
-
-
-                        </div>
-
-
+                    <div className='text-center mb-5 mt-5'>
+                        <Button variant='primary' className='me-2' onClick={() => setShowModal(true)}>Generate Pdf</Button>
                     </div>
                 </div>
-
-                <div className='text-center mb-5 mt-5'>
-                    <Button variant='primary' className='me-2' onClick={() => setShowModal(true)}>Generate Pdf</Button>
+                :
+                <div className=' d-flex justify-content-center align-items-center half-height' >
+                    <h5 className='text-center fs-2 text-secondary my-4 fw-bold error-opacity' >Admit Card is not issued yet</h5>
                 </div>
-
-
-            </div>
+            }
         </div>
     );
 };
--- src/Pages/Students/StudentCourseRegistration/StudentCourseRegistrationDetails/CourseRegistrationView/AdmitCardModal.js
+++ src/Pages/Students/StudentCourseRegistration/StudentCourseRegistrationDetails/CourseRegistrationView/AdmitCardModal.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { FaDownload } from 'react-icons/fa';
-import { Button, Modal, Table } from 'react-bootstrap';
+import { Button, Modal } from 'react-bootstrap';
 import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf';
-// import html2pdf from 'html2pdf.js';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import checkDepartmentName from '../../../../../Functions/DeptCodeToDeptName';
 import mbstuLogo from "../../../../../images/mbstu-logo.jpg"
 
 const AdmitCardModal = (props) => {
+
     const { application, showModal, setShowModal } = props;
 
     const { name, degree, applicantId, applicantName, applicantSession, department,
@@ -26,26 +26,29 @@ const AdmitCardModal = (props) => {
         })
             .then(res => res.json())
             .then(info => {
-                console.log("profile info == ", info)
                 setProfile(info?.data);
             })
     }, [])
 
     const handleDownload = () => {
+
         const selected = document.getElementById('selectedPortion');
+
         const divHeight = selected.clientHeight
         const divWidth = selected.clientWidth
         const ratio = divHeight / divWidth;
 
-        html2canvas(selected, { scale: '5' }).then((canvas) => {
+        html2canvas(selected, { useCORS: true }, { scale: '5' }).then((canvas) => {
             const imgData = canvas.toDataURL('image/jpeg');
-            const pdfDOC = new jsPDF("l", "mm", "a4"); //  use a4 for smaller page
+
+            const pdfDOC = new jsPDF("l", "mm", "a4");
+
             const width = pdfDOC.internal.pageSize.getWidth();
             let height = pdfDOC.internal.pageSize.getHeight();
+
             height = ratio * width;
-            // console.log(height, width, ratio)
-            pdfDOC.addImage(imgData, 'JPEG', 0, 0, width, height, 'FAST');
-            pdfDOC.save("admit-card.pdf");   //Download the rendered PDF.
+            pdfDOC.addImage(imgData, 'JPEG', 0, 0, width, height + 30, 'FAST');
+            pdfDOC.save(`admit-card-${applicantId.toUpperCase()}.pdf`);
         });
     }
     return (
@@ -55,8 +58,6 @@ const AdmitCardModal = (props) => {
                 onHide={() => setShowModal(false)}
                 dialogClassName="modal-90w"
                 aria-labelledby="example-custom-modal-styling-title"
-                // fullscreen={true}
-                // className='m-5'
                 size='xl'
             >
                 <Modal.Header closeButton>
@@ -73,12 +74,12 @@ const AdmitCardModal = (props) => {
                         <div className='px-4'>
                             <div className='row'>
                                 <div className='col-lg-1 mt-2'>
-                                    <img src={mbstuLogo} alt="mbstu" style={{ width: "100px" }} />
+                                    <img src={mbstuLogo} alt="mbstu" style={{ width: "140px" }} />
                                 </div>
-                                <div className="col-lg-9 ps-4 mt-2 font-style-work-sans">
-                                    <h4 className='fw-bold text-center'>Mawlana Bhashani Science and Technology University</h4>
+                                <div className="col-lg-9 ps-5 mt-2 font-style-work-sans">
+                                    <h3 className='fw-bold text-center' style={{ fontSize: "22px" }}>Mawlana Bhashani Science and Technology University</h3>
                                     <p className='text-center'>Santosh, Tangail</p>
-                                    <h3 className='text-center mb-3 fw-bold'>Admit Card</h3>
+                                    <h3 className='text-center fw-bold mb-3'>Admit Card</h3>
                                     <h5 className='text-center'>{name} {degree} Final Examination</h5>
                                 </div>
                                 <div className='col-lg-2'>
@@ -86,7 +87,7 @@ const AdmitCardModal = (props) => {
                                 </div>
                             </div>
 
-                            <div className='d-flex flex-column mb-3 ps-3'>
+                            <div className='d-flex flex-column mb-3 ps-3 mt-3'>
                                 <span style={{ fontSize: "16px" }}><span className="fw-bold">Student ID: </span>{applicantId.toUpperCase()}</span>
                                 <span style={{ fontSize: "16px" }}><span className="fw-bold">Session: </span>{applicantSession}</span>
                                 <span style={{ fontSize: "16px" }}><span className="fw-bold">Name of the applicant: </span>{applicantName}</span>
@@ -108,6 +109,7 @@ const AdmitCardModal = (props) => {
                                             {
                                                 regularCourses?.map(x => <li>{x?.courseCode?.toUpperCase()}</li>)
                                             }
+
                                         </ol>
                                     </div>
                                 }
@@ -139,17 +141,12 @@ const AdmitCardModal = (props) => {
                                         <hr style={{ height: "3px", color: "black", bordr: "none" }} />
                                         <p className='text-center'>Conroller of Examinations</p>
                                     </div>
-
-
                                 </div>
-
-
                             </div>
                         </div>
                     </div>
                 </Modal.Body>
             </Modal>
-
         </div>
     );
 };
